6dd5a31747f00f531ef0bd41484531590d93176a
Katerine Martinez <lp.kathe@gmail.com>
2021-05-08 01:04:43 -0500

    url first function

---
+++ main.js
@@ -0,0 +1,43 @@
+function temperature(response) {
+  const data = response;
+  const temperature = data - 273.15;
+  document.getElementById("asideDegree").innerHTML = `${temperature}º`;
+}
+
+function createURL(city) {
+  const apiKey = "f0110d1ada957ee53f99220d938f65a3";
+  const bogota = "Bogota, co";
+  const paris = "Paris, fr";
+  const lyon = "Lyon, fr";
+  const cityName = bogota;
+
+  if (city === "bogota") {
+    cityName = bogota;
+  } else if (city === "lyon") {
+    cityName = lyon;
+  } else {
+    cityName = paris;
+  }
+
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  return url;
+}
+
+function bogotaInfo() {
+  let request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.send();
+  request.onload = () => {
+    console.log(request);
+
+    if (request.status === 200) {
+      console.log(JSON.parse(request.response));
+      const response = JSON.parse(request.response);
+      temperature(response.main.temp);
+    } else {
+      console.log(`Error ${request.status} ${request.statusText}`);
+    }
+  };
+}
+
+bogotaInfo();
