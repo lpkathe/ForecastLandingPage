c6b704fd7448021809b7175217011b8837547498
Katerine Martinez <lp.kathe@gmail.com>
2021-05-08 17:15:18 -0500

    forecast cards function

--- main.js
+++ main.js
@@ -2,19 +2,53 @@ function onLoad() {
   loadCities();
 }
 
-function callWeatherAPI(url) {
-  let request = new XMLHttpRequest();
-  request.open("GET", url);
-  request.send();
-  request.onload = (request) => {
-    if (request.target.status === 200) {
-      console.log(request.target.response);
-      return request.target.response;
-    } else {
-      console.log(`Error ${request.status} ${request.statusText}`);
-      return "[]";
-    }
-  };
+const get = (url) => {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.send();
+    xhr.onload = () => {
+      if (xhr.status == 200) {
+        resolve(JSON.parse(xhr.responseText));
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    xhr.onerror = () => {
+      reject(xhr.statusText && xhr.status);
+    };
+  });
+};
+
+function assignValues(response) {
+  console.log(response);
+
+  document.getElementById(
+    "asideDegree"
+  ).innerHTML = `${response[0].current.temp}`;
+  document.getElementById(
+    "asideImg"
+  ).src = `http://openweathermap.org/img/wn/${response[0].current.weather[0].icon}@2x.png`;
+  document.getElementById(
+    "asideImgDescription"
+  ).src = `http://openweathermap.org/img/wn/${response[0].current.weather[0].icon}@2x.png`;
+
+  for (index = 0; index < 3; index++) {
+    const card1 = document.getElementById(`forecastCard${index + 1}`);
+    card1.querySelector(
+      ".card__img"
+    ).src = `http://openweathermap.org/img/wn/${response[0].daily[index].weather[0].icon}@2x.png`;
+    card1.querySelector(".text__day").innerText = new Date(
+      response[0].daily[index].dt * 1000
+    ).toLocaleString("en-US", { weekday: "long" });
+    card1.querySelector(".text__weather").innerText =
+      response[0].daily[index].weather[0].main;
+    card1.querySelector(".humidity").innerText =
+      response[0].daily[index].humidity;
+    card1.querySelector(".degrees").innerText = `${Math.round(
+      response[0].daily[index].temp.day
+    )}`;
+  }
 }
 
 function loadCities() {
@@ -34,23 +68,16 @@ function loadCities() {
       lon: 2.349014,
     },
   ];
+  let promiseArr = [];
 
   cities.forEach((data, index) => {
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${apiKey}`;
-    const response = callWeatherAPI(url);
-    //response = JSON.parse(response);
-    console.log(response);
-    temperature(response, index);
-    forecast(response, index);
+    promiseArr.push(get(url));
+  });
+
+  Promise.all(promiseArr).then((response) => {
+    assignValues(response);
   });
 }
-
-function temperature(response, index) {
-  const data = response;
-  const temperature = data - 273.15;
-  document.getElementById("asideDegree").innerHTML = `${temperature}º`;
-}
-
-function forecast(response, index) {}
 
 window.addEventListener("load", onLoad);
